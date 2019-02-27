c7e10e055ad7a7cf4e8095b1451d8b22dfc19b3c
AdrianPerezGarcia <apereg24@estudiantes.unileon.es>
2019-02-27 12:10:15 +0100

    Ejercicio 1 con un bucle de comprobacion como el ejercicio 3.

---
+++ reloj_v2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int main () {
+	long dias, horas, minutos, segundos, dato;
+	printf("Algoritmo que convierte segundos en dias, horas, minutos y segundos.\n");
+	do
+	{
+		printf("Introduce un valor en segundos:\n");
+		scanf("%ld" ,&dato);
+		dias = dato / 86400;
+		horas = (dato % 86400)/3600;
+		minutos = (dato % 3600)/60;
+		segundos = (dato % 60);
+		if (dato >= 86400){
+			printf("El resultado es %ld días, %ld horas, %ld minutos y %ld segundos.\n", dias, horas, minutos, segundos );
+		}
+		else{
+			if (dato >= 3600){
+				printf("El resultado es %ld horas, %ld minutos y %ld segundos.\n", horas, minutos, segundos);
+			}
+			else{
+				if (dato >= 60){
+					printf("El resultado es %ld minutos y %ld segundos.\n", minutos, segundos);
+				}
+				else{ if(dato >= 0){
+					printf("El resultado es %ld segundos.\n", segundos);
+				}
+					else{
+						printf("Introduce un valor valido en segundos.\n \n");
+						printf("Vuelve a introducir los valores:\n");
+					}
+				}
+			}
+		}
+	} while (dato < 0);
+
+
+		return 0;
+
+
+}
